a5a077ddc926e9e18973847a105310a05d078c61
Karl Morris <karl.morris@temple.edu>
2022-09-15 03:32:42 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.temple.inclassuiacvitivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val changeButton = findViewById<Button>(R.id.changeButton)
+        val displayTextView = findViewById<TextView>(R.id.textDisplay)
+
+        // Step 1: Populate array
+        val numberArray = IntArray(100)
+
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numberArray.asList())
+
+
+        // Step 2: Save selected text size
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        // Step 3: Change TextView to saved text size
+        changeButton.setOnClickListener {
+
+        }
+
+    }
+}
